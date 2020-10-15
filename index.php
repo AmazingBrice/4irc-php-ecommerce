87e56171eb3dd04c6eba198f909fb76dc958c44d
vebervi <vincent.veber@cpe.fr>
2020-10-15 16:38:40 +0200

    ADD routing in index.php

--- index.php
+++ index.php
@@ -3,15 +3,6 @@ session_start();
 
 require('./libs/smarty/Smarty.class.php');
 require('./controllers/controller.php');
-require('./models/model.php');
-
-try {
-    $bdd = new PDO("mysql:host=localhost;dbname=webapp","debian-sys-maint","aR7RIRZbiUZw3dYk");
-    $bdd->query("SET NAMES UTF8");
-} catch (Exception$e) {
-    echo"Problème de connexion à la base de donnée !";
-    die();
-}
 
 // Initialisation Smarty
 $smarty = new Smarty();
@@ -20,3 +11,21 @@ $smarty->display('./views/header.tpl');
 // Le corps de la page.
 
 $smarty->display('./views/footer.tpl');
+
+// Fonction de routing.
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listProducts') {
+        listProducts();
+    }
+    elseif ($_GET['action'] == 'displayProduct') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            displayProduct($_GET['id']);
+        }
+        else {
+            echo 'Erreur : aucun identifiant ID renseigné.';
+        }
+    }
+}
+else {
+    listProducts();
+}
