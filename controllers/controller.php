4de25f048e2af65f7205f0323ce5c062f95bb21b
AmazingBrice <brice.folleas@gmail.com>
2020-10-16 11:59:28 +0200

    UPDATE /controllers to have the added queries from /models

--- controllers/controller.php
+++ controllers/controller.php
@@ -5,7 +5,9 @@ require_once('model/BasketManager.php');
 require_once('model/UserManager.php');
 
 function listProducts() {
-    $products = getProducts();
+    $productManager = new ProductManager(); // Création d'un objet
+    $products = $productManager->getProducts(); // Appel d'une fonction de cet objet
+
     echo("products : ");
     var_dump($products);
 
@@ -13,9 +15,28 @@ function listProducts() {
 }
 
 function displayProduct($id) {
-    $product = getProduct($id);
+    $productManager = new ProductManager();
+    $product = $productManager->getProduct($id);
+
     echo("product : ");
     var_dump($product);
 
     //require('displayProductView.php');
 }
+
+function displayInfoByUser($userId) {
+    $userManager = new UserManager();
+    $userInfo = $userManager->getInfoByUser($userId);
+
+    echo("user info : ");
+    var_dump($userInfo);
+}
+
+function displayBasketByCustomer($customer) {
+    $basketManager = new CustomerManager();
+    $basket = $basketManager->getBasketByCustomer($customer);
+
+    echo("basket : ");
+    var_dump($basket);
+}
+
